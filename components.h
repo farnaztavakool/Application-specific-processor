9478d1360f172dda87f8bf8ae5eb6028107218a9
Ava Williams <ava.williams@unsw.edu.au>
2021-04-12 21:44:35 +1000

    Update components.h

--- components.h
+++ components.h
@@ -5,6 +5,13 @@
 
 uint_40 output_memory(u_int16_t dataout, uint8 tag);
 
+// memory is a 56 bit component
+// 0-15:    secret key
+// 16-55:   output data
+//   16-47: data
+//   48-55: tag
+
+
 void input_reg(){
     /*
     input : {clk, reset, datain & tag(20bits), dataout & parity(17 bits)}
