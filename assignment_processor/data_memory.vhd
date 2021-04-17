19e728965aa61086a87b770b7b6d710c37415a45
ash <anran.li@student.unsw.edu.au>
2021-04-17 03:48:46 -0700

    data_mem update

--- assignment_processor/data_memory.vhd
+++ assignment_processor/data_memory.vhd
@@ -29,12 +29,14 @@ entity data_memory is
            write_enable : in  std_logic;
            write_data   : in  std_logic_vector(15 downto 0);
            addr_in      : in  std_logic_vector(3 downto 0);
+           data_net     : out std_logic_vector(31 downto 0);
            data_out     : out std_logic_vector(15 downto 0) );
 end data_memory;
 
 architecture behavioral of data_memory is
 
-type mem_array is array(0 to 15) of std_logic_vector(15 downto 0);
+--type mem_array is array(0 to 15) of std_logic_vector(15 downto 0);
+type mem_array is array(0 to 2) of std_logic_vector(15 downto 0);
 signal sig_data_mem : mem_array;
 
 begin
@@ -51,22 +53,10 @@ begin
         
         if (reset = '1') then
             -- initial values of the data memory : reset to zero 
-            var_data_mem(0)  := X"0000";
+            var_data_mem(0)  := X"aaaa";--keyvalue
             var_data_mem(1)  := X"0000";
             var_data_mem(2)  := X"0000";
-            var_data_mem(3)  := X"0000";
-            var_data_mem(4)  := X"0000";
-            var_data_mem(5)  := X"0000";
-            var_data_mem(6)  := X"0000";
-            var_data_mem(7)  := X"0000";
-            var_data_mem(8)  := X"0000";
-            var_data_mem(9)  := X"0000";
-            var_data_mem(10) := X"0000";
-            var_data_mem(11) := X"0000";
-            var_data_mem(12) := X"0005";
-            var_data_mem(13) := X"0008";
-            var_data_mem(14) := X"0001";
-            var_data_mem(15) := X"0002";
+
 
         elsif (falling_edge(clk) and write_enable = '1') then
             -- memory writes on the falling clock edge
@@ -75,7 +65,7 @@ begin
        
         -- continuous read of the memory location given by var_addr 
         data_out <= var_data_mem(var_addr);
- 
+        data_net <= var_data_mem(1) & var_data_mem(2);
         -- the following are probe signals (for simulation purpose) 
         sig_data_mem <= var_data_mem;
 
