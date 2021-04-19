c3902acd0873b2563d504eda0c5cc4d5834c48fa
Dongzhu Huang <dong.2000@hotmail.com>
2021-04-20 03:05:40 +1000

    Changed key for debug

--- assignment_processor/data_memory.vhd
+++ assignment_processor/data_memory.vhd
@@ -54,7 +54,7 @@ begin
 
         if (reset = '1') then
             -- initial values of the data memory : reset to zero
-            var_data_mem(0)  := X"AAAA"; -- secret key
+            var_data_mem(0)  := X"ABCD"; -- secret key
             var_data_mem(1)  := X"1234";
             var_data_mem(2)  := X"0000";
             var_data_mem(3)  := X"0000";
